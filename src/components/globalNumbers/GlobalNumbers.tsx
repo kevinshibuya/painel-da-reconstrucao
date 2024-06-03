33f1b4bddb7f20811578e716e3dd254c650ead65
Kevin <kevin@flowautobody.com.au>
2024-06-03 15:13:32 -0300

    created transfers page design

--- src/components/globalNumbers/GlobalNumbers.tsx
+++ src/components/globalNumbers/GlobalNumbers.tsx
@@ -6,41 +6,21 @@ import styles from "./globalNumbers.module.scss";
 export default function GlobalNumbers() {
   return (
     <header className={styles.container}>
-      <div className="small_block content_block" id="federalInvestment">
+      <div className="content_block" id="federalInvestment">
         <h1 className="segment_title">Investimento Federal</h1>
         <p className="general_value">R$ 60 bi</p>
       </div>
-      <div className="small_block content_block" id="stateInvestment">
+      <div className="content_block" id="stateInvestment">
         <h1 className="segment_title">Investimento Estadual</h1>
         <p className="general_value">R$ 10 bi</p>
       </div>
-      <div
-        className={classNames(
-          "small_block content_block main_highlight",
-          styles.main_highlight
-        )}
-        id="totalInvestment"
-      >
-        <h1 className={classNames("segment_title", styles.segment_title)}>
-          Repasses totais
-        </h1>
-        <p className={classNames("general_value", styles.general_value)}>
-          R$ 70 bi
-        </p>
+      <div className="content_block main_highlight" id="totalInvestment">
+        <h1 className="segment_title">Repasses totais</h1>
+        <p className="general_value">R$ 70 bi</p>
       </div>
-      <div
-        className={classNames(
-          "small_block content_block sub_highlight",
-          styles.sub_highlight
-        )}
-        id="actualInvestment"
-      >
-        <h1 className={classNames("segment_title", styles.segment_title)}>
-          Valor investido
-        </h1>
-        <p className={classNames("general_value", styles.general_value)}>
-          R$ 2.3 bi
-        </p>
+      <div className="content_block sub_highlight" id="actualInvestment">
+        <h1 className="segment_title">Valor investido</h1>
+        <p className="general_value">R$ 2.3 bi</p>
       </div>
     </header>
   );
